51886ec3f1bda68e2db3049673ff7d77eb6f6e81
denixtorres7 <44268208+denixtorres7@users.noreply.github.com>
2026-06-02 12:36:33 -0500

    Update timo.js

--- timo.js
+++ timo.js
@@ -178,7 +178,7 @@ function actualizarAudioPorEscena(){
 
 function audioPortada(){
   const p = progresoPortada, estres = 1-p, calma = p;
-  setVol("forest", .55*estres); setVol("heartbeatFast", .28*estres); setVol("stressLow", .35*estres); setVol("stressHigh", .18*estres); setVol("stressNoise", .25*estres); setVol("rollingBall", .25*estres); setVol("play_Ball", .25*estres);setVol("fireflies", .06 + .22*calma); setVol("softChimes", .14*calma);
+  setVol("forest", .55*estres); setVol("heartbeatFast", .28*estres); setVol("stressLow", .35*estres); setVol("stressHigh", .18*estres); setVol("stressNoise", .25*estres); setVol("rolling_Ball", .25*estres); setVol("play_Ball", .25*estres);setVol("fireflies", .06 + .22*calma); setVol("softChimes", .14*calma);
   setVol("heartbeatSoft", .03 + .16*calma); setVol("calmAir", .08 + .35*calma); setVol("correr", .08 + .35*calma); 
 }
 function audioRespira(){
